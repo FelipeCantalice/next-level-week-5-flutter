dfc50489daae9fb8cc43e7ac743cbd2271dcd2e3
Felipe Pedro Cantalice <felipep31a@hotmail.com>
2021-04-19 14:08:23 -0400

    Nova tela de inicio, com componentes de AppBar e Chart

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_5/home/home_page.dart';
 import 'package:nlw_5/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "DevQuiz",
       home: SplashPage(),
     );
