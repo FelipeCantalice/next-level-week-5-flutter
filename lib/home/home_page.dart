30c0a41528c9a1415a3b760fb4c6a81333cdcd69
Felipe Pedro Cantalice <felipep31a@hotmail.com>
2021-04-20 13:01:19 -0400

    novos componentes para a pagina home

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -15,25 +15,42 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBarWidget(),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                LevelButtonWidget(
-                  label: "Fácil",
-                ),
-                LevelButtonWidget(
-                  label: "Médio",
-                ),
-                LevelButtonWidget(
-                  label: "Difícil",
-                ),
-                LevelButtonWidget(
-                  label: "Perito",
-                ),
-              ],
+            SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(
+                    label: "Fácil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Médio",
+                  ),
+                  LevelButtonWidget(
+                    label: "Difícil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Perito",
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 24),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
             ),
           ],
         ),
--- lib/home/widgets/quizcard/quizcard_widget.dart
+++ lib/home/widgets/quizcard/quizcard_widget.dart
@@ -26,23 +26,23 @@ class QuizCardWidget extends StatelessWidget {
             height: 40,
             child: Image.asset(AppImages.blocks),
           ),
-          SizedBox(height: 24),
+          SizedBox(height: 20),
           Text(
             "Gerenciamento de Estado",
             style: AppTextStyles.heading15,
           ),
-          SizedBox(height: 24),
+          SizedBox(height: 20),
           Row(
             children: [
               Expanded(
                 flex: 1,
                 child: Text(
-                  "3 de 10",
+                  "3/10",
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
-                flex: 4,
+                flex: 2,
                 child: LinearProgressIndicator(
                   value: 0.3,
                   backgroundColor: AppColors.chartSecondary,
