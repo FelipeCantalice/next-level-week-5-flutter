54d71a1d8771a7c2044d98391360cbc356a1ed56
Felipe Pedro Cantalice <felipep31a@hotmail.com>
2021-04-20 10:37:53 -0400

    novos icones adicionados a home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_5/home/widgets/levelbutton/levelbutton_widget.dart';
 
 import 'widgets/appbar/appbar_widget.dart';
+import 'widgets/quizcard/quizcard_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,6 +14,30 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(
+                  label: "Fácil",
+                ),
+                LevelButtonWidget(
+                  label: "Médio",
+                ),
+                LevelButtonWidget(
+                  label: "Difícil",
+                ),
+                LevelButtonWidget(
+                  label: "Perito",
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
