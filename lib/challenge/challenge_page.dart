3ec56be239fe01bb5771fcf9f93752c35faf887a
Felipe Pedro Cantalice <felipep31a@hotmail.com>
2021-04-22 13:09:42 -0400

    Challenges Page novo controlador e funcionalidades(navegação, pageview)

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,24 +1,95 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nlw_5/challenge/widgets/question_indicator_widget/question_indicator_widget.dart';
 import 'package:nlw_5/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:nlw_5/shared/models/question_model.dart';
+
+import 'challenge_controller.dart';
+import 'widgets/next_button/next_button_widget.dart';
 
 class ChallengePage extends StatefulWidget {
-  ChallengePage({Key? key}) : super(key: key);
+  final List<QuestionModel> questions;
+  ChallengePage({
+    Key? key,
+    required this.questions,
+  }) : super(key: key);
 
   @override
   _ChallengePageState createState() => _ChallengePageState();
 }
 
 class _ChallengePageState extends State<ChallengePage> {
+  final controller = ChallengeController();
+  final pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      controller.currentPage = pageController.page!.toInt() + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
-        child: SafeArea(top: true, child: QuestionIndicatorWidget()),
+        preferredSize: Size.fromHeight(100),
+        child: SafeArea(
+            top: true,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                BackButton(),
+                ValueListenableBuilder<int>(
+                  valueListenable: controller.currentPageNotifier,
+                  builder: (ctx, val, _) => QuestionIndicatorWidget(
+                    currentPage: val,
+                    length: widget.questions.length,
+                  ),
+                ),
+              ],
+            )),
       ),
-      body: QuizWidget(
-        title: "O que o Flutter faz em sua totalidade?",
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: [
+          for (var q in widget.questions) ...{
+            QuizWidget(
+              question: q,
+            )
+          }
+        ],
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: NextButtonWidget.white(
+                  label: "Pular",
+                  onTap: () {
+                    pageController.nextPage(
+                      duration: Duration(milliseconds: 100),
+                      curve: Curves.linear,
+                    );
+                  },
+                ),
+              ),
+              SizedBox(width: 7),
+              Expanded(
+                child: NextButtonWidget.green(
+                  label: "Confirmar",
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nlw_5/core/app_colors.dart';
+
+class NextButtonWidget extends StatelessWidget {
+  final String label;
+  final Color backgroundColor;
+  final Color color;
+  final Color borderColor;
+  final VoidCallback onTap;
+
+  NextButtonWidget({
+    required this.label,
+    required this.backgroundColor,
+    required this.color,
+    required this.borderColor,
+    required this.onTap,
+  });
+
+  NextButtonWidget.green({
+    required String label,
+    required VoidCallback onTap,
+  })   : this.label = label,
+        this.color = AppColors.white,
+        this.backgroundColor = AppColors.darkGreen,
+        this.borderColor = AppColors.green,
+        this.onTap = onTap;
+
+  NextButtonWidget.white({
+    required String label,
+    required VoidCallback onTap,
+  })   : this.label = label,
+        this.color = AppColors.grey,
+        this.backgroundColor = AppColors.white,
+        this.borderColor = AppColors.border,
+        this.onTap = onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            backgroundColor,
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          side: MaterialStateProperty.all(
+            BorderSide(color: borderColor),
+          ),
+        ),
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(
+              fontWeight: FontWeight.w600, fontSize: 15, color: color),
+        ),
+        onPressed: onTap,
+      ),
+    );
+  }
+}
