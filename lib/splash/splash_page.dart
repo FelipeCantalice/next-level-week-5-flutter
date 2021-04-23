d7b8a8f90977f34cd8e294578a545a76f6606814
Felipe Pedro Cantalice <felipep31a@hotmail.com>
2021-04-23 08:30:51 -0400

    Novas páginas adicionadas

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:nlw_5/core/app_gradients.dart';
 import 'package:nlw_5/core/core.dart';
+import 'package:nlw_5/home/home_page.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // navegar para a home page
+    Future.delayed(Duration(seconds: 2)).then(
+      (value) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (ctx) => HomePage(),
+        ),
+      ),
+    );
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(gradient: AppGradients.linear),
